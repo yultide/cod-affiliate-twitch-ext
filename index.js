475c19c76492c9c63182ca9ed8466064b4d4ea8c
Yulius Tjahjadi <yultide@yahoo.com>
2018-08-10 00:27:45 -0700

    add set-extension-required-configuration request

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const AWS = require('aws-sdk'),
     dynamoDbDoc = new AWS.DynamoDB.DocumentClient(),
     dynamoDb = new AWS.DynamoDB();
-    IS_CORS = true;
 const jwt = require('jsonwebtoken');
+const request = require('request');
 const LambdaReq = require('lambda-req').default;
 const LambdaReqError = require('lambda-req').LambdaReqError;
 const lambda = new LambdaReq()
 const bearerPrefix = "Bearer ";
 const secret = process.env.TWITCH_SECRET;
+const configSecret = process.env.TWITCH_CONFIG_SECRET;
+const clientId = process.env.TWITCH_CLIENT_ID;
+const clientVersion = process.env.TWITCH_CLIENT_VERSION;
+const ownerUserId = process.env.TWITCH_USER_ID;
 
 function processResponse(isCors, body, statusCode) {
     const status = statusCode || (body ? 200 : 404);
@@ -33,7 +37,7 @@ function verifyAndDecode(header) {
             const token = header.substring(bearerPrefix.length);
             console.log('token', token);
             console.log('secret', secret);
-            var secret_buffer  = Buffer(secret, 'base64');
+            var secret_buffer = new Buffer(secret, 'base64');
             return jwt.verify(token, secret_buffer, { algorithms: ['HS256'] });
         }
         catch (ex) {
@@ -82,7 +86,7 @@ lambda.get('/api/get/{userid}', (req, router)=> {
             .then(response => {
                 console.log('/api/get/', requestedItemId, response.Item);
                 if (response.Item)
-                    resolve(response.Item)
+                    resolve(response.Item);
                 else
                     reject(error('Not found', 404));
             })
@@ -99,6 +103,33 @@ lambda.post('/api/save', (req, router)=> {
     if (opaqueUserId != req.params.userid) {
         throw error('Invalid credentials', 500);
     }
+
+    // https://dev.twitch.tv/docs/extensions/reference/#set-extension-required-configuration
+    // set the required configuration value
+    var url = `https://api.twitch.tv/extensions/${clientId}/${clientVersion}/required_configuration?channel_id=${channelId}`
+    var ebs_token = {
+        "exp": parseInt(Date.now()/1000) + 3600,
+        "user_id": ownerUserId,
+        "role": "external"
+    }
+    var secret_buffer = new Buffer(secret, 'base64');
+    var encoded_ebs_token = jwt.sign(ebs_token, secret_buffer, { algorithm: 'HS256' });
+    request.put({
+        url: url,
+        headers: {
+            'Authorization': 'Bearer ' + encoded_ebs_token,
+            'Client-Id': clientId
+        },
+        body: {
+            'required_configuration': configSecret
+        },
+        json: true
+    }).on('response', function(response) {
+        console.log('set-extension-required-configuration response', response.statusCode); // 200
+        response.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+         });
+    });
 
     return new Promise((resolve, reject) => {
         var tableName = "CodAffiliate";
@@ -131,4 +162,4 @@ lambda.post('/api/save', (req, router)=> {
 
 // export the handler
 // pass the event params on invocation time
-module.exports = { handler: lambda.invoke }
+module.exports = { handler: lambda.invoke };
